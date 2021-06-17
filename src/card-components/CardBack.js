45bb11f32ab27db22e8f70bbd165a5190fbc28ed
Zhino Salam <zhinosalam188@gmail.com>
2021-06-17 21:38:57 +0000

    Automatically backed up by Learn

--- src/card-components/CardBack.js
+++ src/card-components/CardBack.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+<<<<<<< HEAD
 import zero from '../assets/stars/0-stars.png';
 import one from '../assets/stars/1-stars.png';
 import two from '../assets/stars/2-stars.png';
@@ -28,3 +29,33 @@ export default class CardBack extends Component {
 		);
 	}
 }
+=======
+import zero from '../assets/stars/0-stars.png'
+import one from '../assets/stars/1-stars.png'
+import two from '../assets/stars/2-stars.png'
+import three from '../assets/stars/3-stars.png'
+import four from '../assets/stars/4-stars.png'
+import five from '../assets/stars/5-stars.png'
+const imgMapper = {0: zero, 1: one, 2: two, 3: three, 4: four, 5: five}
+export default class CardBack extends Component {
+
+  generateRatingElement = () => {
+  } {
+ 		return <img src={imgMapper[this.props.IMDBRating]} alt="" />;
+ 		}
+ 		return <h4>No Rating Found</h4>;
+    }
+
+  render() {
+    return (
+      <div className="card-back">
+      <h3 className="title">{this.props.title}</h3>
+        <span />
+      {this.generateRatingElement()}
+        <span />
+        <h5 className="genres">{this.props.genres.join(', ')}</h5>
+      </div>
+    )
+  }
+  }
+>>>>>>> 725eeb37634c688c102a1c4f42436a46cb000770
